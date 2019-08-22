b27d259ed935b040cea1f9ed69bdbd5d7f3f12e8
caianmei <1610143991@qq.com>
2019-08-22 20:57:48 +0800

    Practice03

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,31 @@
 package practice03;
 
-public class Student {
+import practice03.Person;
+
+public class Student extends Person{
+
+	private int klass;
+	
+	public int getKlass() {
+		return klass;
+	}
+	public void setKlass(int klass) {
+		this.klass = klass;
+	}
+	
+	public Student(String name, int age,int klass) {
+		super(name, age);
+		this.klass = klass;
+	}
+	
+	/**
+	 * functionName:introduce方法
+	 * param 
+	 * return String
+	 * **/
+	@Override
+	public String introduce() {
+		return "I am a Student. I am at Class "+ this.klass +".";
+	}
 }
+
